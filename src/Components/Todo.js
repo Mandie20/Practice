d456c80dbd6694d70f63ca08a959854a93655bb8
Mandie20 <jsamandina@gmail.com>
2023-05-26 02:45:42 -0400

    Header desingn

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Todo = () => {
     return (
-        <div>
-            <h3>Sam_s Todo</h3>
-            <button>Create Task</button>
-        </div>
+        <>
+            <div className="header text-center">
+                <h3>Sam_s Todo</h3>
+                <button className='btn btn-primary'>Create Task</button>
+            </div>
+            <div className = "Task-container"></div>
+        </>
     );
 };
 
