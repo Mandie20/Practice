7c5a5d46770ac35bd4d00296ae4c0b07c8bbb597
Mandie20 <jsamandina@gmail.com>
2023-05-26 01:25:18 -0400

    Add new files

---
+++ src/Components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div>
+            <h3>Sam_s Todo</h3>
+            <button>Create Task</button>
+        </div>
+    );
+};
+
+export default Todo;
