02c3d752b4b95dad0d10d8ea7770132046f97aad
Mandie20 <jsamandina@gmail.com>
2023-05-26 20:45:32 -0400

    Create task

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,9 +3,19 @@ import CreateTask from '../modals/createTask'
 
 const Todo = () => {
     const [modal, setModal] = useState(false);
+    // to get information task
+    const [taskList, setTaskList] = useState([]);
 
     const toggle = () => {
         setModal(modal);
+    }
+
+    // fonction to get task information
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        setTaskList(taskList)
+        setModal(false)
     }
 
     return (
@@ -14,10 +24,10 @@ const Todo = () => {
                 <h3>Sam_s Todo</h3>
                 <button className="btn btn-primary mt-2" onClick={ () => setModal(true)}>Create Task</button>
             </div>
-            <div className = "Task-container">
-
+            <div className = "task-container">
+                {taskList.map((obj) => <li>{obj.Name}</li>)}
             </div>
-            <CreateTask toggle = {toggle} modal = {modal}/>
+            <CreateTask toggle = {toggle} modal = {modal} save = {saveTask} />
         </>
     );
 };
